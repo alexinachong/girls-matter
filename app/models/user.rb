729872cc18596f22721c9e59d7fdb43188c7f887
Alexina Chong <alexina.chong@gmail.com>
2018-09-26 20:02:19 -0700

    Add User model validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :password, length: { minimum: 8 }, allow_nil: true
+  validates :password_digest, presence: true
+  validates :session_token, presence: true
+
+  
 end
